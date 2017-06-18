39ff20dad601f21bb0ac7aa0c1ac153355cb40fc
Jayson Rondina <jmr_mymsaccnt@hotmail.com>
2017-06-18 16:43:30 +0800

    RQM v1.1

--- quote.js
+++ quote.js
@@ -12,32 +12,28 @@ function getQuoteFn() {
     $.ajax({
 
         url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=', // The URL to the API.
-
         type: 'GET',
-
         //data: {}, // Additional parameters here
-
         dataType: 'json',
-
         success: function(data) {
+            var dtq = data.quote;
+            var dta = data.author;
+            var attrValue = "https://twitter.com/intent/tweet?text=" + dtq + " - " + dta;
 
             console.log(data);
 
-            $("#quoteBox").html("<h3 class='flow-text'>" + data.quote + "</h3>");
-            $("#quoteAuthor").html("<em><h6 class='flow-text blue-text text-lighten-4'>- " + data.author + "</h6></em>");
-
-            // document.getElementById("quote").innerHTML = data.source;
+            $("#tweetLink").prop("href",attrValue);
+            $("#quoteBox").html("<h3 class='flow-text'>" + dtq + "</h3>");
+            $("#quoteAuthor").html("<h6 class='flow-text blue-text text-darken-1'>- " + dta + "</h6>");
+           
         },
-
         error: function(err) {
-            alert(err);
+            console.log(err);
+        },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "li7CFnVytCmshu5vqwd6FMFiEyoXp1Wv4u2jsnnSx2D8MFAHFF"); // Mashape key
         },
 
-        beforeSend: function(xhr) {
-
-            xhr.setRequestHeader("X-Mashape-Authorization", "li7CFnVytCmshu5vqwd6FMFiEyoXp1Wv4u2jsnnSx2D8MFAHFF"); // Mashape key
-
-        }
+        
     });
 }
-
